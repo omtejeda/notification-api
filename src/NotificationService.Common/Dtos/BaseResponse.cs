66863d535a28f6cac33438f278f0079f525b01f4
omtejeda <omarky12-@hotmail.com>
2024-08-29 19:35:11 -0400

    refact: static methods for response dto instances

--- src/NotificationService.Common/Dtos/BaseResponse.cs
+++ src/NotificationService.Common/Dtos/BaseResponse.cs
@@ -1,30 +1,28 @@
-using static NotificationService.Common.Utils.ResponseUtil;
-
 namespace NotificationService.Common.Dtos
 {
     public class BaseResponse<T> where T : class
     {
-        public BaseResponse(int code, T data = null)
+        public BaseResponse(int code, T? data = null)
         {
-            Response = GetResponseObject(code);
+            Response = ResponseDto.CreateResponse(code);
             Data = data;
         }
 
         public BaseResponse(int code, T data, PaginationDto pagination)
         {
-            Response = GetResponseObject(code);
+            Response = ResponseDto.CreateResponse(code);
             Data = data;
             Pagination = pagination;
         }
 
-        public BaseResponse(int code, string message, T data = null)
+        public BaseResponse(int code, string message, T? data = null)
         {
-            Response = GetResponseObject(code, message);
+            Response = ResponseDto.CreateResponse(code, message);
             Data = data;
         }
 
         public ResponseDto Response { get; private set; }
-        public PaginationDto Pagination { get; set; }
-        public T Data { get; private set; }
+        public PaginationDto? Pagination { get; set; }
+        public T? Data { get; private set; }
     }
 }
--- src/NotificationService.Common/Dtos/ResponseDTO.cs
+++ src/NotificationService.Common/Dtos/ResponseDTO.cs
@@ -1,11 +1,38 @@
 using System;
+using NotificationService.Common.Enums;
+using NotificationService.Common.Utils;
 namespace NotificationService.Common.Dtos
 {
     public class ResponseDto
     {
         public bool? Success { get; set; }
         public int? Code { get; set; }
-        public string Message { get; set; }
+        public string? Message { get; set; }
         public DateTime? Date { get; set; }
+
+        private ResponseDto() {}
+
+        public static ResponseDto CreateResponse(int code)
+        {
+            return CreateResponse(code, message: ((ResultCode) code).ToString());
+        }
+
+        public static ResponseDto CreateResponse(int code, string message)
+        {
+            return GetInstance(code, message);
+        }
+
+        private static ResponseDto GetInstance(int code, string? message)
+            => new()
+            {
+                Success = IsSuccess(code),
+                Code = code,
+                Message = message ?? string.Empty,
+                Date = SystemUtil.GetSystemDate()
+            };
+        
+        private static bool IsSuccess(int code)
+            => code % 10 == 0;
+        
     }
 }
