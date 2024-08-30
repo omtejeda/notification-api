75c40fa0dea1cc366baddc15e184fb67ccf1275f
omtejeda <omarky12-@hotmail.com>
2024-08-29 20:08:00 -0400

    refact: removing non used result codes

--- src/NotificationService.Common/Enums/ResultCode.cs
+++ src/NotificationService.Common/Enums/ResultCode.cs
@@ -7,22 +7,8 @@ namespace NotificationService.Common.Enums
         Warning = 2,
         AccessDenied = 3,
         ValidationError = 4,
-
-        EmailOK = 10,
         EmailNotSent = 11,
-        EmailPending = 12,
-
-
-        TemplateOK = 20,
-        TemplateDoesNotExist = 21,
-        TemplateInvalid = 22,
-
-        PlatformOK = 30,
-        PlatformNotActive = 31,
-        PlatformInvalid = 32,
-
         HttpRequestOK = 40,
-        HttpRequestNotSent = 41,
-        HttpRequestPending = 42
+        HttpRequestNotSent = 41
     }
 }
