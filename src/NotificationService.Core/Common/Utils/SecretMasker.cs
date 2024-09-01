0490f807b60f979a75aa5dcac2511ab18b7d96f2
omtejeda <omarky12-@hotmail.com>
2024-09-01 18:31:57 -0400

    refact: secret masker

--- src/NotificationService.Core/Common/Utils/SecretMasker.cs
+++ src/NotificationService.Core/Common/Utils/SecretMasker.cs
@@ -1,22 +1,28 @@
-using System;
 namespace NotificationService.Core.Common.Utils;
 
 public static class SecretMasker
 {
+    private const int MaxVisibleCharacter = 4;
+    private const char MaskCharacter = '*';
+
+    /// <summary>
+    /// Masks a secret string by keeping a certain number of characters visible on each end and masking the rest.
+    /// </summary>
+    /// <param name="secret">The secret string to be masked.</param>
+    /// <returns>The masked string.</returns>
     internal static string Mask(string secret)
     {
-        int maxVisibleCharacter = 4;
-        int visibleCharacters = Math.Min(secret.Length / 2, maxVisibleCharacter);
+        int visibleCharacters = Math.Min(secret.Length / 2, MaxVisibleCharacter);
         int totalVisibleCharacters = visibleCharacters * 2;
 
         if (secret.Length <= totalVisibleCharacters)
-            return secret;
-        
-        char maskCharacter = '*';
+        {
+            return new string(MaskCharacter, secret.Length);
+        }
         
         string firstPart = secret[..visibleCharacters];
         string lastPart = secret[^visibleCharacters..];
-        string middleMask = new(maskCharacter, secret.Length - totalVisibleCharacters);
+        string middleMask = new(MaskCharacter, secret.Length - totalVisibleCharacters);
 
         return $"{firstPart}{middleMask}{lastPart}";
     }
