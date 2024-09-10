7e56adf725ffe6f48464613d9e6c61ea2151f23c
omtejeda <omarky12-@hotmail.com>
2024-09-10 19:58:07 -0400

    refact: whitespace

--- src/NotificationService.Api/Options/LocalizationOptions.cs
+++ src/NotificationService.Api/Options/LocalizationOptions.cs
@@ -2,6 +2,6 @@ namespace NotificationService.Api.Options;
 
 public record LocalizationOptions
 {
-    public string DefaultCulture{ get; set; }
+    public string DefaultCulture { get; set; }
     public string[] SupportedCultures { get; set; }
 }
