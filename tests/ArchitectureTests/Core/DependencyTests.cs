d26425aaa9900c3e5bc90b7be71787b0373b5283
omtejeda <omarky12-@hotmail.com>
2024-09-10 21:46:32 -0400

    feat: architecture tests

---
+++ tests/ArchitectureTests/Core/DependencyTests.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using NetArchTest.Rules;
+using NotificationService.Core.Senders;
+
+namespace NotificationService.ArchitectureTests.Core;
+
+public class DependencyTests
+{
+    private static readonly Assembly CoreAssembly = typeof(EmailSender).Assembly;
+    [Fact]
+    public void Core_ShouldNotHaveDepdencyOnUpperLayers()
+    {
+        var result = Types
+            .InAssembly(CoreAssembly)
+            .ShouldNot()
+            .HaveDependencyOnAny("NotificationService.Api")
+            .GetResult();
+        Assert.True(result.IsSuccessful);
+    }
+}
