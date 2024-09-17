7778d9fdcfbcf1af157d4cde47125aeba64c5460
omtejeda <omarky12-@hotmail.com>
2024-09-16 21:59:24 -0400

    refact: create platform with non null values

--- src/NotificationService.Application/Features/Platforms/Commands/Create/CreatePlatformCommand.cs
+++ src/NotificationService.Application/Features/Platforms/Commands/Create/CreatePlatformCommand.cs
@@ -5,7 +5,7 @@ namespace NotificationService.Application.Features.Platforms.Commands.Create;
 
 public class CreatePlatformCommand : ICommand<BaseResponse<PlatformDto>>
 {
-    public string? Name { get; init; }
-    public string? Description { get; init; }
-    public string? Owner { get; set; }
+    public string Name { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+    public string Owner { get; set; } = string.Empty;
 }
