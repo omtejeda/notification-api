62bb547bcc1653079245affa1423337a96da0880
omtejeda <omarky12-@hotmail.com>
2024-09-19 20:33:12 -0400

    refact; removed LimitPageSize env variable

--- src/NotificationService.Application/Common/Services/EnvironmentService.cs
+++ src/NotificationService.Application/Common/Services/EnvironmentService.cs
@@ -7,7 +7,6 @@ public class EnvironmentService : IEnvironmentService
 {
     private const string EnvironmentKey = "ASPNETCORE_ENVIRONMENT";
     private const string GmtTimezoneKey = "GMT_TIMEZONE";
-    private const string LimitPageSizeKey = "LIMIT_PAGE_SIZE";
 
     public string? CurrentEnvironment
         => Environment.GetEnvironmentVariable(EnvironmentKey)?.ToString();
@@ -20,9 +19,6 @@ public class EnvironmentService : IEnvironmentService
     
     public int? GmtOffset
         => GetIntEnvironmentVariable(GmtTimezoneKey);
-    
-    public int? LimitPageSize
-        => GetIntEnvironmentVariable(LimitPageSizeKey);
     
     private static int? GetIntEnvironmentVariable(string name)
     {
--- src/NotificationService.Common/Interfaces/IEnvironmentService.cs
+++ src/NotificationService.Common/Interfaces/IEnvironmentService.cs
@@ -5,5 +5,4 @@ public interface IEnvironmentService
     string? CurrentEnvironment { get; }
     bool IsProduction { get; }
     int? GmtOffset { get; }
-    int? LimitPageSize { get; }
 }
