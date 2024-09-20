3abed44f6cfca0520e775f62fe576d446e15c2d0
omtejeda <omarky12-@hotmail.com>
2024-09-19 23:08:47 -0400

    refact: appUtil

--- src/NotificationService.Common/Utils/AppUtil.cs
+++ src/NotificationService.Common/Utils/AppUtil.cs
@@ -4,12 +4,14 @@ public static class AppUtil
 {
     public static int? Gmt
         => GetIntEnvironmentVariable("GMT_TIMEZONE");
+    
     public static DateTime CurrentDate
         => DateTime.Now.AddHours(Gmt.GetValueOrDefault());
 
-    private static int? GetIntEnvironmentVariable(string name)
+    private static int GetIntEnvironmentVariable(string name)
     {
-        var gotten = int.TryParse(Environment.GetEnvironmentVariable(name), out var value);
-        return gotten ? value : default(int?);
+        return int.TryParse(Environment.GetEnvironmentVariable(name), out var value)
+        ? value
+        : default;
     }
 }
