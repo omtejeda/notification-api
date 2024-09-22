66009e29388eafe296a17cd971a8910f55652f57
omtejeda <omarky12-@hotmail.com>
2024-09-22 14:59:42 -0400

    feat: publishing provider events

--- src/NotificationService.Application/Features/Providers/Commands/Create/CreateProviderCommandHandler.cs
+++ src/NotificationService.Application/Features/Providers/Commands/Create/CreateProviderCommandHandler.cs
@@ -2,17 +2,22 @@ using NotificationService.SharedKernel.Interfaces;
 using NotificationService.Application.Contracts.Interfaces.Services;
 using NotificationService.Application.Common.Dtos;
 using NotificationService.Application.Common.Models;
+using MediatR;
+using NotificationService.Application.Features.Providers.Events.Created;
 
 namespace NotificationService.Application.Features.Providers.Commands.Create;
 
-public class CreateProviderCommandHandler(IProviderService providerService) 
+public class CreateProviderCommandHandler(IProviderService providerService, IMediator mediator) 
     : ICommandHandler<CreateProviderCommand, BaseResponse<ProviderDto>>
 {
     private readonly IProviderService _providerService = providerService;
+    private readonly IMediator _mediator = mediator;
 
     public async Task<BaseResponse<ProviderDto>> Handle(CreateProviderCommand request, CancellationToken cancellationToken)
     {
         var result = await _providerService.CreateProvider(request.RequestDto, request.Owner);
+
+        await _mediator.Publish(new ProviderCreatedEvent(result.Data));
         return result;
     }
 }
