48aca3729e3eb90c63bc7cd9fc4c527d2b474f4e
omtejeda <omarky12-@hotmail.com>
2024-09-24 16:40:07 -0400

    chore: add XML summary documentation to EnvironmentConstants

--- src/NotificationService.SharedKernel/Constants/EnvironmentConstants.cs
+++ src/NotificationService.SharedKernel/Constants/EnvironmentConstants.cs
@@ -1,6 +1,12 @@
 namespace NotificationService.SharedKernel.Constants;
 
+/// <summary>
+/// Provides constants related to the application environment.
+/// </summary>
 public static class EnvironmentConstants
 {
+    /// <summary>
+    /// The name of the production environment.
+    /// </summary>
     public const string ProductionName = "Production";
 }
