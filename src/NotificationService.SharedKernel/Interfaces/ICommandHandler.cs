f1f2ef2a132fd3de09408b782caf23724e351c0a
omtejeda <omarky12-@hotmail.com>
2024-09-24 16:37:02 -0400

    chore: add XML summary documentation to ICommandHandler interface

--- src/NotificationService.SharedKernel/Interfaces/ICommandHandler.cs
+++ src/NotificationService.SharedKernel/Interfaces/ICommandHandler.cs
@@ -2,11 +2,20 @@ using MediatR;
 
 namespace NotificationService.SharedKernel.Interfaces;
 
+/// <summary>
+/// Represents a handler for a command that returns a response.
+/// </summary>
+/// <typeparam name="TCommand">The type of the command being handled.</typeparam>
+/// <typeparam name="TResponse">The type of the response returned by the command.</typeparam>
 public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
     where TCommand : ICommand<TResponse>
 {
 }
 
+/// <summary>
+/// Represents a handler for a command that does not return a response.
+/// </summary>
+/// <typeparam name="TCommand">The type of the command being handled.</typeparam>
 public interface ICommandHandler<TCommand> : IRequestHandler<TCommand>
     where TCommand : ICommand
 {
