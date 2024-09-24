3633d01043c79be9ed2a41a89b57d21bb72cc353
omtejeda <omarky12-@hotmail.com>
2024-09-24 16:35:30 -0400

    chore: add XML summary documentation to IEnvironmentService interface

--- src/NotificationService.SharedKernel/Interfaces/IEnvironmentService.cs
+++ src/NotificationService.SharedKernel/Interfaces/IEnvironmentService.cs
@@ -1,8 +1,22 @@
 namespace NotificationService.SharedKernel.Interfaces;
 
+/// <summary>
+/// Provides information about the current environment in which the application is running.
+/// </summary>
 public interface IEnvironmentService
 {
+    /// <summary>
+    /// Gets the name of the current environment (e.g., Development, Staging, Production).
+    /// </summary>
     string? CurrentEnvironment { get; }
+
+    /// <summary>
+    /// Indicates whether the application is running in a production environment.
+    /// </summary>
     bool IsProduction { get; }
+
+    /// <summary>
+    /// Gets the GMT offset of the current environment in hours.
+    /// </summary>
     int? GmtOffset { get; }
 }
