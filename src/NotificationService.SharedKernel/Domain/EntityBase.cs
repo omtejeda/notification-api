372e42f11fc72b2f4294f8879da7216096cf6073
omtejeda <omarky12-@hotmail.com>
2024-09-24 16:40:39 -0400

    chore: add XML summary documentation to EntityBase

--- src/NotificationService.SharedKernel/Domain/EntityBase.cs
+++ src/NotificationService.SharedKernel/Domain/EntityBase.cs
@@ -1,11 +1,37 @@
 namespace NotificationService.SharedKernel.Domain;
 
+/// <summary>
+/// Represents a base entity with common properties for all entities.
+/// </summary>
 public abstract class EntityBase
 {
+    /// <summary>
+    /// Gets or sets the unique identifier for the entity.
+    /// </summary>
     public virtual string Id { get; protected set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the date and time when the entity was created.
+    /// </summary>
     public virtual DateTime? CreatedOn { get; set; }
+
+    /// <summary>
+    /// Gets or sets the identifier of the user who created the entity.
+    /// </summary>
     public virtual string CreatedBy { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the date and time when the entity was last modified.
+    /// </summary>
     public virtual DateTime? ModifiedOn { get; set; }
+
+    /// <summary>
+    /// Gets or sets the identifier of the user who last modified the entity.
+    /// </summary>
     public virtual string? ModifiedBy { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the entity is deleted.
+    /// </summary>
     public virtual bool? Deleted { get; set; } = false;
 }
