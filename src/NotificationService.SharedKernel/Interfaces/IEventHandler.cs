9e7690abd96e741d621b12e6f614adc69f7a80ef
omtejeda <omarky12-@hotmail.com>
2024-09-24 16:38:05 -0400

    chore: add XML summary documentation to IEventHandler interface

--- src/NotificationService.SharedKernel/Interfaces/IEventHandler.cs
+++ src/NotificationService.SharedKernel/Interfaces/IEventHandler.cs
@@ -2,6 +2,10 @@ using MediatR;
 
 namespace NotificationService.SharedKernel.Interfaces;
 
+/// <summary>
+/// Defines a handler for processing events of type <typeparamref name="TEvent"/>.
+/// </summary>
+/// <typeparam name="TEvent">The type of event that this handler processes.</typeparam>
 public interface IEventHandler<TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
 {
 }
