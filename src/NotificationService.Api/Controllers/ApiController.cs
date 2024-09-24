69374e884ae5b3adcead4341735bd7cbd6caf0b1
omtejeda <omarky12-@hotmail.com>
2024-09-24 12:45:24 -0400

    chore: remove unused method

--- src/NotificationService.Api/Controllers/ApiController.cs
+++ src/NotificationService.Api/Controllers/ApiController.cs
@@ -13,8 +13,5 @@ namespace NotificationService.Api.Controllers
                 return HttpContext.Items[nameof(PlatformDto)] as PlatformDto;
             }
         }
-
-        protected IActionResult GetActionResult<T>(BaseResponse<T> response) where T : class 
-            => (response?.Data is null) ? NotFound() : Ok(response);
     }
 }
