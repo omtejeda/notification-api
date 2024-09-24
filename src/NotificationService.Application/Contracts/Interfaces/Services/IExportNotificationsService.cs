c41759432d147e2116b08fe9bdad0a618edfc6ea
omtejeda <omarky12-@hotmail.com>
2024-09-24 15:56:20 -0400

    chore: add XML summary documentation to IExportNotificationsService interface

--- src/NotificationService.Application/Contracts/Interfaces/Services/IExportNotificationsService.cs
+++ src/NotificationService.Application/Contracts/Interfaces/Services/IExportNotificationsService.cs
@@ -1,11 +1,24 @@
 using NotificationService.Application.Contracts.ResponseDtos;
 using NotificationService.Domain.Enums;
 
-namespace NotificationService.Application.Contracts.Interfaces.Services;
-
-public interface IExportNotificationsService
+namespace NotificationService.Application.Contracts.Interfaces.Services
 {
-    ExportFormat ExportFormat { get; }
+    /// <summary>
+    /// Defines the contract for exporting notifications.
+    /// </summary>
+    public interface IExportNotificationsService
+    {
+        /// <summary>
+        /// The format in which the notification will be exported.
+        /// </summary>
+        ExportFormat ExportFormat { get; }
 
-    Task<ExportNotificationResponseDto> Export(string notificationId, string owner);
+        /// <summary>
+        /// Exports a notification based on its identifier and the owner's information.
+        /// </summary>
+        /// <param name="notificationId">The unique identifier of the notification to be exported.</param>
+        /// <param name="owner">The owner of the notification.</param>
+        /// <returns>An <see cref="ExportNotificationResponseDto"/> containing the export details.</returns>
+        Task<ExportNotificationResponseDto> Export(string notificationId, string owner);
+    }
 }
