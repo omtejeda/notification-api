d81973116fd684fd95a905c8f29dda240e57447f
omtejeda <omarky12-@hotmail.com>
2024-09-24 16:37:39 -0400

    chore: add XML summary documentation to IEvent interface

--- src/NotificationService.SharedKernel/Interfaces/IEvent.cs
+++ src/NotificationService.SharedKernel/Interfaces/IEvent.cs
@@ -2,6 +2,9 @@ using MediatR;
 
 namespace NotificationService.SharedKernel.Interfaces;
 
+/// <summary>
+/// Represents an event that can be published through the MediatR library.
+/// </summary>
 public interface IEvent : INotification
 {
 }
