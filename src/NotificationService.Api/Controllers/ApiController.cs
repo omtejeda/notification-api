37b61edc77f137059cabbf8e9eb4571d0b7c4790
omtejeda <omarky12-@hotmail.com>
2024-09-24 16:45:52 -0400

    chore: add XML summary documentation to ApiController

--- src/NotificationService.Api/Controllers/ApiController.cs
+++ src/NotificationService.Api/Controllers/ApiController.cs
@@ -3,9 +3,16 @@ using NotificationService.Application.Common.Dtos;
 
 namespace NotificationService.Api.Controllers
 {   
+    /// <summary>
+    /// Serves as a base class for API controllers, 
+    /// providing common functionality and shared properties for derived controllers.
+    /// </summary>
     [ApiController]
-    public class ApiController() : ControllerBase
+    public class ApiController : ControllerBase
     {
+        /// <summary>
+        /// Gets the current platform from the HTTP context.
+        /// </summary>
         protected PlatformDto CurrentPlatform
         { 
             get
