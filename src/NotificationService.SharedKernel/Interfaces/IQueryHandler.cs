06e735331d740d720f782dcf860fa1385ac38b1e
omtejeda <omarky12-@hotmail.com>
2024-09-24 16:39:37 -0400

    chore: add XML summary documentation to IQueryHandler interface

--- src/NotificationService.SharedKernel/Interfaces/IQueryHandler.cs
+++ src/NotificationService.SharedKernel/Interfaces/IQueryHandler.cs
@@ -2,6 +2,11 @@ using MediatR;
 
 namespace NotificationService.SharedKernel.Interfaces;
 
+/// <summary>
+/// Represents a handler for processing queries of type <typeparamref name="TQuery"/> that return a response of type <typeparamref name="TResponse"/>.
+/// </summary>
+/// <typeparam name="TQuery">The type of the query that this handler processes.</typeparam>
+/// <typeparam name="TResponse">The type of the response that this query returns.</typeparam>
 public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
     where TQuery : IQuery<TResponse>
 {
