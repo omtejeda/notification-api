0ef4f85a150d18deeec333b2cdd57bbc203ae981
omtejeda <omarky12-@hotmail.com>
2024-09-24 16:33:30 -0400

    chore: add XML summary documentation to LocalizationOptions

--- src/NotificationService.Api/Options/LocalizationOptions.cs
+++ src/NotificationService.Api/Options/LocalizationOptions.cs
@@ -1,3 +1,9 @@
 namespace NotificationService.Api.Options;
 
+/// <summary>
+/// Represents the localization options for the application.
+/// This record is used to capture the Localization section in the appsettings.json file.
+/// </summary>
+/// <param name="DefaultCulture">The default culture for localization.</param>
+/// <param name="SupportedCultures">An array of supported cultures for localization.</param>
 public record LocalizationOptions(string DefaultCulture, string[] SupportedCultures);
