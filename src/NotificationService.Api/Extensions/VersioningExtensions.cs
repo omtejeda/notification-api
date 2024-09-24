f0787c01e56f7b77adfcd43bb5c7252030bea4d4
omtejeda <omarky12-@hotmail.com>
2024-09-24 16:26:38 -0400

    chore: add XML summary documentation to VersioningExtensions

--- src/NotificationService.Api/Extensions/VersioningExtensions.cs
+++ src/NotificationService.Api/Extensions/VersioningExtensions.cs
@@ -2,8 +2,17 @@
 
 namespace NotificationService.Api.Extensions;
 
+/// <summary>
+/// Provides extension methods for configuring API versioning in the application.
+/// </summary>
 public static class VersioningExtensions
 {
+    /// <summary>
+    /// Configures API versioning services for the application.
+    /// This method sets the default API version, ensures the default version is assumed when not specified,
+    /// and enables reporting of API versions.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the versioning services to.</param>
     public static void AddVersioning(this IServiceCollection services)
     {
         services.AddApiVersioning(config =>
@@ -11,7 +20,6 @@ public static class VersioningExtensions
             config.DefaultApiVersion = new ApiVersion(1, 0);
             config.AssumeDefaultVersionWhenUnspecified = true;
             config.ReportApiVersions = true;
-
         });
 
         services.AddVersionedApiExplorer(setup =>
